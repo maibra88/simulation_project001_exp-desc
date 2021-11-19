fcf43fd74980cf80c275a8ab02e4e721fa2d2207
maibra88 <94648993+maibra88@users.noreply.github.com>
2021-11-19 11:27:42 +0100

    version0_simulation
    This is the very first version of my simulation exercise which will later be useful for my PhD project. The aim is to simulate data in an experience-description paradigm which will later be linked to the preference reversal. The approach is a neuropsychological one.

---
+++ prefrev_expdesc_data_simulation.R
@@ -0,0 +1,14 @@
+# functions for model fitting
+# Experience-description gap & preference reversal v0-1
+# Created by Maike Brandt, Maike Brandt, Zurich, 11/2021
+
+
+# MODELS ------------------------------------------------------------------
+
+# Models of interest:
+  # expected utility theory (EUT)
+  # cumulative prospect theory (CPT)
+  # salience theory (ST)
+  # Q-learning reinforcement (RI-Q)
+
+# simulate non-random choice data for training
